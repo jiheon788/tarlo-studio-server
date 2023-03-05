912a87b126e9c42896ebc608d576a3a32fbe51cb
jiheon788 <jiheon788@ajou.ac.kr>
2023-03-05 16:29:09 +0900

    feat: implement t2i api and set cors

--- README.md
+++ README.md
@@ -4,6 +4,13 @@
 
 Open your browser at [http://127.0.0.1:8000](http://127.0.0.1:8000). You will see the automatic interactive [API documentation](http://127.0.0.1:8000/docs)
 
+#### Fill out `.env`
+
+```
+KAKAOBRAIN_HOST_URL=${kakaobrain host url}
+KAKAO_REST_API_KEY=${your kakao rest api key}
+```
+
 #### Install
 
 ```
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from tarlo import t2i
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -8,6 +22,6 @@ async def root():
     return {"message": "tarlo server open"}
 
 
-@app.get("/designs")
-async def root():
-    return {"message": "tarlo server open"}
+@app.get("/designs/")
+async def root(text: str, size: int):
+    return t2i(text, size)
